d3cd5046575658fc41a6b7d4ca54bf4267f21ef7
Amelie <AmelieinInternet@163.com>
2022-09-15 22:26:35 +0800

    1. 添加数据库用户创建语句 2. 更新图片

--- MySQL/AmelieChat.sql
+++ MySQL/AmelieChat.sql
@@ -7,3 +7,8 @@ CREATE TABLE `user`(
     `username` VARCHAR(20) UNIQUE NOT NULL,
     `password` VARCHAR(40) NOT NULL
 );
+
+-- 以下3行用来创建用户，用户名和密码都是 chat
+-- CREATE USER 'chat'@'%' IDENTIFIED BY 'chat';
+-- GRANT ALL ON `AmelieChat`.* TO 'chat'@'%';
+-- flush privileges;
